73a3fe3301dac7bde7e5b3ade44c1f9ae4e76494
GuiDrakeen <gui.duel@gmail.com>
2023-06-13 21:38:54 -0300

    classe autor recriada

--- apps/autor/models.py
+++ apps/autor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    nacionality = models.CharField('Nacionalidade', max_length=50)
+    
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
